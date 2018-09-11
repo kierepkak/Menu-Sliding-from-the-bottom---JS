8f2673823fd7b0733c43603b280e9da3265bfbc0
KierepkaE <kierepkaedyta@gmail.com>
2018-09-11 17:54:45 +0100

    meu sliding from the bottom - JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+//Java Script
+
+
+
+const arrow = document.querySelector(".arrow");
+const nav = document.querySelector("nav");
+
+arrow.addEventListener("click", function() {
+    arrow.classList.toggle("on");
+    nav.classList.toggle("on");
+})
